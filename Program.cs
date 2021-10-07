116507d52c3e83ed48fc9e2a43d6c138c56317d3
Vegtd63 <90941113+Vegtd63@users.noreply.github.com>
2021-10-07 13:57:18 -0500

    Added Comment and isDiagonal method stub

--- Program.cs
+++ Program.cs
@@ -4,8 +4,22 @@ namespace ConsoleApp1
 {
     class Program
     {
+      
         static void Main(string[] args)
-        { 
+        {
+            
+            
+        }
+        /// <summary>
+        /// This method checks if all the elements are greater 
+        /// than zero. If the number of rows = the number of columns
+        /// and if both things are true, return true, false otherwise.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static bool isDiagonal(int[,] array)
+        {
+            return false;
         }
     }
 }
