fbc8e409ac9058723920ed8ed48425417d1ec139
Sebastian Vang <vangs95@uwosh.edu>
2021-10-07 14:02:55 -0500

    Completed method function

--- Program.cs
+++ Program.cs
@@ -12,14 +12,46 @@ namespace ConsoleApp1
         }
         /// <summary>
         /// This method checks if all the elements are greater 
-        /// than zero. If the number of rows = the number of columns
+        /// than zero. If the number of rows =  the number of columns
         /// and if both things are true, return true, false otherwise.
         /// </summary>
         /// <param name="array"></param>
         /// <returns></returns>
         public static bool isDiagonal(int[,] array)
         {
-            return false;
+            Boolean isDiagonal = true;
+            Boolean isSquare = false;
+
+            //Check if array is empty
+            if (array.Length == 0)
+            {
+                isDiagonal = false;
+            }
+            else
+            {
+                //Check if the array is square
+                int rowLength = array.GetLength(0);
+                int colLength = array.GetLength(1);
+
+                if (rowLength == colLength)
+                {
+                    isSquare = true;
+                }
+
+                //Check if all elements are greater than 0
+                for (int i = 0; i < rowLength; i++)
+                {
+                    for (int j = 0; j < colLength; j++)
+                    {
+                        if (array[i, j] < 1)
+                        {
+                            isDiagonal = false;
+                        }
+                    }
+                }
+            }
+
+            return isDiagonal & isSquare;
         }
     }
 }
